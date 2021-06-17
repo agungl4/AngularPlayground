c6614e9c74d9c5262b7a6c6dca8c02a3084f7f2e
Arif Nur Agung Laksana <agung.arifnur@gmail.com>
2021-06-17 21:38:03 +0700

    importing shared module on appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from './pages/home/navbar/navbar.component';
 import { HeaderComponent } from './pages/home/header/header.component';
 import { ContentComponent } from './pages/home/content/content.component';
 import { FooterComponent } from './pages/home/footer/footer.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { FooterComponent } from './pages/home/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
